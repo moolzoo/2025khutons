64a85a56ade479d1167aa398ca363f87140a5739
moolzoo <seojun050109@gmail.com>
2025-05-09 18:28:05 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,27 @@ import './App.css'
 import CardStack from './CardStack'
 import potatoImg from './assets/images/potato.png';
 
-function App() {
-  const [count, setCount] = useState(0)
+function ResultScreen() {
 
   return (
-    <div className="App">
-      <div style={{ width:"200px",height:"200px",borderRadius:"200px",border:"2px solid black",marginRight:"400px"}}>
+    <div className="result-screen">
+      <div style={{ width:"200px",height:"200px",borderRadius:"200px",border:"2px solid black",marginRight:"300px",left:"300px"}}>
         <img src={potatoImg} alt="Potato" style={{ width: "100%", height: "100%", borderRadius: "200px" }} />
       </div>
       <CardStack></CardStack>
+    </div>
+  );
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+
+  return (
+    <div className="App">
+      <ResultScreen></ResultScreen>
+      <p>hi</p>
     </div>
   )
 }
